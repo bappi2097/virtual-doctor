db86b45e1fa38c9e39f39b33cfebf4f9903cda9b
bappi2097 <bappi35-2097@diu.edu.bd>
2021-04-13 20:26:46 +0600

    auth added

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
-const mix = require("laravel-mix");
-require("laravel-mix-purgecss");
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,22 +10,7 @@ require("laravel-mix-purgecss");
  | file for the application as well as bundling up all the JS files.
  |
  */
-// mix.extract();
 
-mix.js("resources/js/app.js", "public/js")
-    .js("resources/js/frontend/frontend.js", "public/js/frontend.js")
-    .copy("resources/js/backend/toastr.js", "public/js/toastr.js")
-    .copy("resources/js/flickity.js", "public/js")
-    .copy("resources/js/jquery.min.js", "public/js")
-    .copy("resources/js/popper.min.js", "public/js")
-    .copy("resources/js/bootstrap.min.js", "public/js")
-    .copy("resources/js/bootstrap-select.js", "public/js/bootstrap-select.js")
-    .sass("resources/sass/app.scss", "public/css/app.css")
-    .postCss("resources/css/frontend/frontend.css", "public/css/frontend.css")
-    .postCss("resources/css/bootstrap-select.css", "public/css/bootstrap-select.css")
-    .postCss("resources/css/backend/toastr.css", "public/css/toastr.css")
-    .postCss("resources/css/frontend/style-rtl.css", "public/css/style-rtl.css")
-    .postCss("resources/css/frontend/dashboard.css", "public/css/dashboard.css")
-    .copy("resources/images/img", "public/images")
-    .purgeCss()
-    .version();
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
